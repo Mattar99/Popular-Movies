de9ca429ec8812089d1d3ecd7612bb073ad57eb6
Ahmed Mattar <AceMatar@gmail.com>
2018-04-22 00:43:34 +0200

    implemented a Model Class

--- app/src/main/java/com/example/mattar/popular_movies/Model/MainResponse.java
+++ app/src/main/java/com/example/mattar/popular_movies/Model/MainResponse.java
@@ -1,11 +1,59 @@
 package com.example.mattar.popular_movies.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Mattar on 4/22/2018.
  */
 
 public class MainResponse {
 
+    @SerializedName("page")
+    @Expose
+    private int page;
+    @SerializedName("total_results")
+    @Expose
+    private int totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private int totalPages;
+    @SerializedName("results")
+    @Expose
+    private List<MoviesResponse> results = null;
 
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<MoviesResponse> getResults() {
+        return results;
+    }
+
+    public void setResults(List<MoviesResponse> results) {
+        this.results = results;
+    }
 
 }
